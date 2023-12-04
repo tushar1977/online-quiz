a9fe4fb119de6cf3de672be8f2e65056473855ee
tushar1977 <steve192g@gmail.com>
2023-12-04 15:02:02 +0530

    changes

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
-# from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+# from flask_migrate import Migrate
 
 
 app = Flask(__name__ , static_folder="static/")
